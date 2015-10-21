e944279848d5f9e424b8cad1e9a5dd5db6404a32
Miguel Cabrera <miguelcabsan@gmail.com>
2015-10-21 17:57:22 +0100

    Ejercicio 3 y 4 resuelto

---
+++ Actividades4-2/Ejercicio4/ejercicio4.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Ejercicio 4</title>
+</head>
+<body>
+	<form action="resultado_ejercicio4.php" method="post">
+	<label for="textNombre">Usuario:</label><input type="text" id="textUsuario" name="user"/><br>
+	<label for="textApellidos">Contraseña:</label><input type="password" id="textContrasenia" name="password"/><br>
+	<input type="submit" value="Iniciar sesión" />
+	</form>
+</body>
+</html>
--- Actividades4-2/ejercicio3.php
+++ Actividades4-2/ejercicio3.php
@@ -1,1 +1,51 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8" />
+	<title>Ejercicio 4</title>
+</head>
+<body>
+	<form action="ejercicio3.php" method="post">
+	<label for="textPrecio0">Precio 0:</label><input type="text" id="textPrecio0" name="datos[]"/><br>
+	<label for="textPrecio1">Precio 1:</label><input type="text" id="textPrecio1" name="datos[]"/><br>
+	<label for="textPrecio2">Precio 2:</label><input type="text" id="textPrecio2" name="datos[]"/><br>
+	<input type="submit" value="Recoger datos" />
+	</form>
+	<?php
+	function convertirEurosAUSD($euro) {
+		return $euro * 1.29710;
+	}
+	
+	function convertirEurosAGBP($euro) {
+		return $euro * 0.807292;
+	}
+	
+	function convertirEurosAJPY($euro) {
+		return $euro * 106.518;
+	}
+	
+	function construirTablaConversion($datos) {
+		echo "<table border='1'>";
+		echo "<tr bgcolor='grey'><td>Euros</td><td>USD</td><td>GBP</td><td>JPY</td></tr>";
+		for($i = 0; $i < count($datos); $i++) {
+			$aUSD = convertirEurosAUSD($datos[$i]);
+			$aGBP = convertirEurosAGBP($datos[$i]);
+			$aJPY = convertirEurosAJPY($datos[$i]);
+			echo "<tr>";
+			echo "<td>" . sprintf("%05.2f", $datos[$i]) . "</td>";
+			echo "<td>" . sprintf("%05.2f", $aUSD) . "</td>";
+			echo "<td>" . sprintf("%05.2f", $aGBP) . "</td>";
+			echo "<td>" . sprintf("%05.2f", $aJPY) . "</td>";
+			echo "</tr>";
+		}
+	}
+	
+	if(isset($_POST["datos"])) {
+		$datos = $_POST["datos"];
+		construirTablaConversion($datos);
+	}
+	else
+		construirTablaConversion(Array (0));
+	?>
+</body>
+</html>
